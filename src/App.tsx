35ea3697d6909503e93dc4ff5a7d85927bd47dfc
itzhan <2802965114@qq.com>
2024-05-26 08:51:51 +0800

    右媒体组件

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import MediaL from "./components/MediaL";
 import Home from "./components/Home";
 import { Box, Text } from "@chakra-ui/react";
+import MediaR from "./components/MediaR";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Home></Home>
       <Box bg={"#f1f1f1"}>
         <MediaL></MediaL>
+        <MediaR></MediaR>
       </Box>
       <Text mt={'1000'}>1</Text>
     </>
--- src/components/MediaR.tsx
+++ src/components/MediaR.tsx
@@ -37,8 +37,17 @@ const MediaL = () => {
         bgRepeat="no-repeat"
         bgSize="cover"
       >
-        <SimpleGrid templateColumns="1fr 1fr" w={'100vw'} h={'88vh'}>
-          <Box pl="100" alignContent={'center'}>
+        <SimpleGrid templateColumns="1fr 1fr" w={"100vw"} h={"95vh"}>
+          <Box position="relative" overflow="hidden">
+            <Box width="70%" mx="auto">
+              <Image
+                objectFit="cover"
+                src="https://s3-eu-west-3.amazonaws.com/cm-site/wp-content/uploads/2018/03/10162708/01-XVI-600x381.jpg"
+                style={{ transform: `translateY(${scrollY * 0.1}px)` }}
+              />
+            </Box>
+          </Box>
+          <Box pr="100" alignContent={"center"}>
             <Stack spacing={10}>
               <Text color="#cab57e" fontSize="20px" mt="10">
                 地理优势
@@ -47,7 +56,12 @@ const MediaL = () => {
                 在风土一词的概念中，饱含法国传统的葡萄酒文化信息，在其他语言中难以找到与其意义相称的词汇。风土是优质葡萄酒的遗传精髓，没有了它，一切都不复存在。但是，只有人们的辛勤耕作与发奋图强，风土的个性方可得到真正的张扬与提升。
               </Text>
               <Collapse in={isExpand}>
-                <Text  mb={'8'} fontSize="20px" lineHeight="30px" color={"black"}>
+                <Text
+                  mb={"8"}
+                  fontSize="20px"
+                  lineHeight="30px"
+                  color={"black"}
+                >
                   事实上，如果没有人们这种高度的关注与热情，那么任何一片格雷夫石子地山丘，即便它有多么得天独厚，也不会成为一个优质葡萄园。选择最合适的葡萄品种，确定葡萄植株的生长条件，完善酿造和陈化工艺，人们整整用了五百多年摸索出一套培育高品质葡萄酒的好方法。
                 </Text>
               </Collapse>
@@ -55,7 +69,7 @@ const MediaL = () => {
                 <Button
                   w={"225px"}
                   h={"70px"}
-                  mb={'8'}
+                  mb={"8"}
                   fontSize={"16px"}
                   color={"#cab57e"}
                   bg={"#000000"}
@@ -66,15 +80,6 @@ const MediaL = () => {
                 </Button>
               )}
             </Stack>
-          </Box>
-          <Box position="relative" overflow="hidden">
-            <Box width='70%' mx='auto' mt={'130px'}>
-              <Image
-                objectFit="cover"
-                src="https://s3-eu-west-3.amazonaws.com/cm-site/wp-content/uploads/2018/03/10162708/01-XVI-600x381.jpg"
-                style={{ transform: `translateY(${scrollY * 0.1}px)` }}
-              />
-            </Box>
           </Box>
         </SimpleGrid>
       </Box>
