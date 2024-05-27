7539e7acc42ffdb6db5cccd16ff35fa094a67797
itzhan <2802965114@qq.com>
2024-05-27 12:44:36 +0800

    添加：制作流程模块

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
+import { Box } from "@chakra-ui/react";
 import "./App.css";
-import BorderAnimation from "./components/Border";
-import Home from "./components/Home";
-import BeerCard from "./components/shop/BeerCard";
 import BeerShop from "./components/shop/BeerShop";
-import TerroirPage from "./components/terroir/TerroirPage";
-import { Box, Center } from "@chakra-ui/react";
+import Timeline from "./components/timeline/Timeline";
 
 function App() {
   return (
-    <BeerShop></BeerShop>
+    <Box bg={'#333333'} padding={'20'}>
+      <Timeline />
+    </Box>
   );
 }
 
